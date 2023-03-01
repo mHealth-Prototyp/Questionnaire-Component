5adccc99549bc8009469cccc2c1ee09dc1461b04
Fahrni Alex <alex.fahrni@bfh.ch>
2023-03-01 15:32:08 +0100

    beta.17

--- src/components/controls/FQBoolean.vue
+++ src/components/controls/FQBoolean.vue
@@ -1,26 +1,14 @@
 <script setup lang="ts">
 import type {IAnswerOption, IQuestion} from '@i4mi/fhir_questionnaire';
 import {QToggle} from 'quasar';
-import {onUpdated} from 'vue';
 import {useFQInject} from '../FQComposables';
+
 export type FQBooleanProps = {
   question: IQuestion;
 };
 
 const props = defineProps<FQBooleanProps>();
 const {language, onAnswer} = useFQInject();
-setInitialValue();
-onUpdated(() => {
-  setInitialValue();
-});
-
-function setInitialValue() {
-  if (props.question.selectedAnswers.length === 0) {
-    if (props.question.initial && props.question.initial.length > 0) {
-      updateModel(props.question.initial.at(0)?.valueBoolean);
-    }
-  }
-}
 
 function updateModel(value: boolean | undefined) {
   if (value !== undefined) {
